7275630f262d6c0914d54c6ecb31811223df4810
Fahmi Azzuhri Efki <fahmiazzuhriefki@gmail.com>
2026-06-01 16:44:57 +0700

    refactor : change path logo and animate title

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ function App() {
       <div className="max-w-6xl mx-auto p-4 sm:p-10">
         {!result && (
           <div className="text-center mb-12">
-            <h1 className="text-4xl sm:text-4xl font-bold mb-2 finger-paint-regular text-red-500">
+            <h1 className="text-4xl sm:text-4xl font-bold mb-2 finger-paint-regular text-red-500 animate-pulse">
               Roast My Squad
             </h1>
             <div className="flex gap-2 flex-row justify-center mb-4">
